8e1f298969af1d15a64ce79c4f6473d3c80ca9e3
Mathias Pürzl <kontakt@dasgutezeichen.de>
2012-05-23 14:20:26 +0200

    Seed für das Projekt »eScience« erstellt

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+ Project.create 	:name => "eScience",
+					:description => "Das Hauptprojekt der eScience Forschungsplattform, dem sich jeder Benutzer der Community anschließt",
+					:homepage => "http://nochnichtvergeben.de",
+					:is_public => 1,
+					:identifier => "escience",
+					:status => 1
