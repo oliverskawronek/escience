4bdb1ce717373880db6d006dadc5c9582fb71bd9
Christian Reichmann <creichma@imn.htwk-leipzig.de>
2012-07-04 13:15:57 +0200

    migration: changes alter sql string

--- db/migrate/20120702105356_change_default_for_language.rb
+++ db/migrate/20120702105356_change_default_for_language.rb
@@ -1,13 +1,16 @@
 class ChangeDefaultForLanguage < ActiveRecord::Migration
   def self.up
-    change_table :users do |t| 
-      t.change :language, :default => "de"
+    if !(ActiveRecord::Base.connection.instance_values["config"][:adapter].include?("sqlite"))
+      execute "ALTER TABLE users ALTER language SET DEFAULT 'de';"
+      #change_table :users do |t|
+      #  t.change :language, :null => false, :default => 'de'
+      #end
     end
   end
 
   def self.down
-    change_table :users do |t| 
-      t.change :language, :default => ""
+    if !(ActiveRecord::Base.connection.instance_values["config"][:adapter].include?("sqlite"))
+      execute "ALTER TABLE users ALTER language SET DEFAULT '';"
     end
   end
 end
