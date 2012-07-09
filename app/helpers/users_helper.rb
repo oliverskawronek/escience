f2e3bdefbdbe18d14182cfd713edbd0fb37f2968
Christian Reichmann <creichma@imn.htwk-leipzig.de>
2012-07-09 13:24:42 +0200

    contact_list: refactored and now available for everyone, or so i think

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -62,9 +62,9 @@ module UsersHelper
   end
   
   def member_tabs
-    tabs = [{:name => 'members', :partial => 'users/members', :label => :label_memberlist_all},
-            {:name => 'membersgrouped', :partial => 'users/membersgrouped', :label => :label_memberlist_grouped},
-            {:name => 'membersearch', :partial => 'users/membersearch', :label => :label_memberlist_search}
+    tabs = [{:name => 'contact_members', :partial => 'users/contact_members', :label => :label_memberlist_all},
+            {:name => 'contact_membersgrouped', :partial => 'users/contact_membersgrouped', :label => :label_memberlist_grouped},
+            {:name => 'contact_membersearch', :partial => 'users/contact_membersearch', :label => :label_memberlist_search}
             ]
     if Group.all.any?
       tabs.insert 1, {:name => 'groups', :partial => 'users/groups', :label => :label_group_plural}
